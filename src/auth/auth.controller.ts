ed67b72b5f138136f2b2e28c0c6ffa61b4f456d8
Mahmoud-Sanad <82536865+Mahmoud-Sanad@users.noreply.github.com>
2023-07-08 19:17:52 +0300

    add zego

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthDto } from './dto/auth.dto.js';
-
+import { Request } from 'express';
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
+  @Post('send')
+  send(@Req() req: Request) {
+    return this.authservice.sendToZegoServer(req);
+  }
   @Post('register')
   register(@Body() dto: AuthDto) {
     return this.authservice.register(dto);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,76 @@
-import { Injectable, Req, ForbiddenException } from '@nestjs/common';
+import { Injectable, Req, ForbiddenException, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { JwtService } from '@nestjs/jwt';
+import * as crypto from 'crypto';
+
+import axios from 'axios';
+import { Request } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  getZegoSignature(appId, SignatureNonce, serverSecret, timestamp) {
+    const hash = crypto.createHash('md5');
+    const str = appId + SignatureNonce + serverSecret + timestamp;
+    hash.update(str);
+    return hash.digest('hex');
+  }
+  async sendToZegoServer(request: Request) {
+    const RoomId = request.body.RoomId;
+    const userId = request.body.userId;
+    const AppId = 918272161;
+    const serverSecret = '52c2b55f2793f65e66a0b204e67767f8';
+    const timeStamp = Math.round(Date.now() / 1000);
+    const randomBytes = (length) => {
+      const randomUuid = uuidv4();
+      return randomUuid.replace(/-/g, '').substr(0, length * 2);
+    };
+
+    const SignatureNonce = randomBytes(8);
+    const Signature = this.getZegoSignature(
+      AppId,
+      SignatureNonce,
+      serverSecret,
+      timeStamp,
+    );
+    console.log(Signature);
+
+    const url =
+      'https://zim-api.zego.im/?Action=SendRoomMessage&AppId=' +
+      AppId +
+      '&Timestamp=' +
+      timeStamp +
+      '&Signature=' +
+      Signature +
+      '&SignatureVersion=2.0&SignatureNonce=' +
+      SignatureNonce;
+    console.log(url);
+    try {
+      const response: any = await axios({
+        url: url,
+        method: 'POST',
+        data: {
+          FromUserId: userId,
+          RoomId: RoomId,
+          MessageType: 2,
+          Priority: 3,
+          MessageBody: {
+            Message: 'hello world',
+            ExtendedData: 'd',
+          },
+        },
+        // responseEncoding: 'utf-8',
+        // responseType:'json',
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
   async login(dto: AuthDto) {
     console.log(dto);
 
